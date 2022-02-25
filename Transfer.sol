f1ebdd0e29a3fd97db0eabe2b9593ae65b7fde66
Danil Sitdikov <quadrad4@gmail.com>
2022-02-25 16:04:58 +0600

    Transfer contract init

---
+++ Transfer.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.7.0;
+
+contract Transfer {
+    uint public allReceivedMoney;
+
+    function receiveMoney() public payable {
+        allReceivedMoney += msg.value;
+    }
+
+    function getBalance () public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function getAddress () public view returns (address) {
+        address currentAddress = address(this);
+        return currentAddress;
+    }
+
+    function withDraw() public {
+        address payable sender = msg.sender;
+        sender.transfer(getBalance());
+    }
+
+    function transferTo (address payable _to) public {
+        _to.transfer(getBalance());
+    }
+}
